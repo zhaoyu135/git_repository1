2bcfd1672d20b91392ac87ca145e052418f75edb
zhaoyu <470852543@qq.com>
2021-10-26 11:31:18 +0800

    测试

---
+++ src/main/java/com/example/git_repository/GitRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_repository/GitRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
